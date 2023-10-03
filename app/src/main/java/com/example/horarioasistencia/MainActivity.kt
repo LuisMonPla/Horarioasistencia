8f80c3f7ab0090c72125445b75ccbb6472dbb34b
LucheMP <luismonpla@outlook.com>
2023-10-02 19:03:13 -0600

    Initial commit

--- app/src/main/java/com/example/horarioasistencia/MainActivity.kt
+++ app/src/main/java/com/example/horarioasistencia/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.horarioasistencia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val passwordEditText = findViewById<EditText>(R.id.txtpassword)
+        val submitButton = findViewById<FloatingActionButton>(R.id.btnbuscar)
+
+        // Reemplaza "1234" con tu clave correcta
+        val claveCorrecta = "1234"
+
+        submitButton.setOnClickListener {
+            val inputClave = passwordEditText.text.toString()
+
+            if (inputClave == claveCorrecta) {
+                // La clave es correcta, abre la nueva ventana
+                val intent = Intent(this, Menuopciones::class.java)
+                startActivity(intent)
+            } else {
+                // La clave es incorrecta, muestra un mensaje de error
+                val mensajeError = "Contraseña incorrecta"
+                val duracionToast = Toast.LENGTH_SHORT // Puedes usar Toast.LENGTH_LONG si deseas que dure más tiempo
+
+                val toast = Toast.makeText(this, mensajeError, duracionToast)
+                toast.show()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/horarioasistencia/Menuopciones.kt
+++ app/src/main/java/com/example/horarioasistencia/Menuopciones.kt
@@ -2,10 +2,42 @@ package com.example.horarioasistencia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.View
+import android.widget.Button
 
 class Menuopciones : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menuopciones)
+
+        val btnHorario = findViewById<Button>(R.id.btnhorario)
+        val btnAgregar = findViewById<Button>(R.id.btnagregar)
+        val btnModificar = findViewById<Button>(R.id.btnmodificar)
+        val btnVolver = findViewById<Button>(R.id.btnvolver)
+
+        // Configurar OnClickListener para el botón btnHorario
+        btnHorario.setOnClickListener {
+            val intent = Intent(this, Horario::class.java)
+            startActivity(intent)
+        }
+
+        // Configurar OnClickListener para el botón btnAgregar
+        btnAgregar.setOnClickListener {
+            val intent = Intent(this, Agregarempleado::class.java)
+            startActivity(intent)
+        }
+
+        // Configurar OnClickListener para el botón btnModificar
+        btnModificar.setOnClickListener {
+            val intent = Intent(this, modificarempleado::class.java)
+            startActivity(intent)
+        }
+
+        // Configurar OnClickListener para el botón btnVolver
+        btnVolver.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
